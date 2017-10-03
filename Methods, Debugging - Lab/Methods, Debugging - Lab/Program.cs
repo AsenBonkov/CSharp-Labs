a27d80379a189fb32ca698056eae808f86cc07e7
AsenBonkov <asenbonkov@abv.bg>
2017-10-03 12:42:09 +0300

    !

--- Methods, Debugging - Lab/Methods, Debugging - Lab/Program.cs
+++ Methods, Debugging - Lab/Methods, Debugging - Lab/Program.cs
@@ -6,6 +6,16 @@ namespace MathPower
     {
         static void Main(string[] args)
         {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine(RaiseToPower(numberOne, numberTwo));
+        }
+
+        static double RaiseToPower(double number, int power)
+        {
+            double result = 0d;
+            result = Math.Pow(number, power);
+            return result;    
         }
     }
 }
